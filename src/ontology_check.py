be2a4c9e9067a4afe0ea6c274425a9377b2169f6
Connor Macdonald <connor@bkmac.ca>
2024-10-21 14:01:02 +0200

    fix the reasoner

--- src/ontology_check.py
+++ src/ontology_check.py
@@ -7,20 +7,21 @@ class OntologyCheck:
     def __init__(self):
         my_world = World()
         my_world.get_ontology("test_ont.rdf").load()
-        sync_reasoner(my_world)  # reasoner is started and synchronized here
+        sync_reasoner(
+            my_world, infer_property_values=True
+        )  # reasoner is started and synchronized here)  # reasoner is started and synchronized here
         self.graph = my_world.as_rdflib_graph()
 
     def ontology_check_truth(self, user_input: str) -> IsTruth:
 
         query = """
-                PREFIX : <http://www.semanticweb.org/jip/ontologies/2024/9/untitled-ontology-23/>
+                PREFIX : <http://www.semanticweb.org/jip/ontologies/2024/9/untitled-ontology-23#>
                 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
                 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
                 PREFIX owl: <http://www.w3.org/2002/07/owl#>
-                    SELECT ?sport
+                    SELECT ?x
                     WHERE {
-                        ?sport rdf:type/rdfs:subClassOf :Sport .                           # Find instances of Sport
-                    
+                        :ramen :contains ?x .
                     }
                 """
 
