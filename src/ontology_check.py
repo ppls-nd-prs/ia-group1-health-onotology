55343d3f7fdfa01e403136a54724a1d390c1d803
jips99 <jipschoneveld@gmail.com>
2024-10-25 23:08:46 +0200

    check if in ontology

--- src/ontology_check.py
+++ src/ontology_check.py
@@ -1,4 +1,6 @@
 from owlready2 import *
+from rdflib import URIRef
+from rdflib.namespace import RDF
 
 from src.queries import *
 from src.utils import IsTruth
@@ -7,6 +9,8 @@ import re
 
 
 class OntologyCheck:
+    prefix = "http://www.semanticweb.org/uu/ia/group1/health/ontology#"
+
     symptoms = '(?:an |a )?(\w*(?: \w*)?)(?: and (?:an |a )?(\w*(?: \w*)?))?' #!in report: 1-2 symptoms 
     sport = '(?:play |perform |do )?(.*)'
     sporting = '(?:playing |performing |do )?(.*)'
@@ -37,26 +41,25 @@ class OntologyCheck:
                     arg2 = ['_'.join(a.split(' ')) for a in p.groups()[1:] if a is not None] #remove optional groups that were not filled 
                 else: 
                     arg2 = '_'.join(p.groups()[1].split(' '))
-                if self.check_if_in_ontology(pattern[1], arg1, arg2):
+                if self.check_if_in_ontology(pattern[1], arg1, arg2, verbose):
                     if verbose: print('>>>QUERY:', pattern[1], arg1, arg2)
                     return query_functions[pattern[1]](self.graph, arg1, arg2, verbose)
         print('Unknown query, please try again.')
         return 
   
-    def check_if_in_ontology(self, pattern, arg1, arg2):
-        return True #! should be removed 
-        classes = re.findall(r'\b[A-Z][a-z]*\b', pattern)
-        class1 = self.my_world.classes[0]
-        class2 = self.my_world.classes[1]
-        if not class1 in self.my_world.arg1.is_a:
-            return False 
-        
-        if type(arg2) == list: 
-            for a in arg2: 
-                if not class2 in self.my_world.a.is_a:
-                    return False
-        else: 
-            if not class2 in self.my_world.arg2.is_a:
+    def check_if_in_ontology(self, pattern, arg1, arg2, verbose = False):
+        classes = re.findall(r'[A-Z][a-z]*', pattern)
+        # check if in onottlogy at all 
+        for a, c in zip([arg1, arg2], classes):
+            if verbose: print('>>>CHECKING:', a, c )
+            a_ont = URIRef(self.prefix + a)
+            if not (a_ont, RDF.type, None) in self.graph:
+                print(f"There is no knowledge available about {a}. To perform any reasoning about this concept, the ontolgy should be updated.")
                 return False
-
-
+            # TODO fix this 
+            # c_ont = URIRef(self.prefix + c) 
+            # if not (a_ont, RDF.type, c_ont) in self.graph:
+            #     print(f"Unable to proceed because {a} is not an instance of {c}.")
+            #     return False
+            
+        return True 
