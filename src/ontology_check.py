e5c975dbd5eaa9e2ee50cf915c60955e33adfde2
jips99 <jipschoneveld@gmail.com>
2024-10-25 18:03:50 +0200

    fixed pattern not_sport

--- src/ontology_check.py
+++ src/ontology_check.py
@@ -9,17 +9,17 @@ import re
 class OntologyCheck:
     symptoms = '(?:an |a )?(\w*(?: \w*)?)(?: and (?:an |a )?(\w*(?: \w*)?))+'
     sport = '(?:play |perform )?(.*)'
-    sporting = '(?:playing |performing )?(.*)'
+    sporting = '(?:playing |performing|do )?(.*)'
 
     Recipe_by_Health = (re.compile('Does eating (.*) cause (.*)\?'), 'Recipe_by_Health')
-    Sport_and_not_Sport = (re.compile(f'Can not being able to {sport} be caused by {sporting}\?'), 'Sport_and_not_Sport')
+    not_Sport_and_Sport = (re.compile(f'Can not being able to do {sporting} be caused by {sporting}\?'), 'not_Sport_and_Sport')
     Sport_promotes_over_Sport = (re.compile(f'Does {sporting} provide any health benefit that is not provided by {sporting}\?'), ) 
     Recipe_fuels_Sport = (re.compile(f'Does (.*) provide the required nutrients for {sporting}\?'), 'Sport_promotes_over_Sport')
     Allergy_eat_Recipe = (re.compile(f'Can someone with (?:an|a) (.*) eat (.*)\?'), 'Allergy_eat_Recipe')
     Recipe_help_Symptom = (re.compile(f'Does eating (.*) help against {symptoms}\?'), 'Recipe_help_Symptom')
     Sport_with_Symptom = (re.compile(f'Is it possible that you are able to {sport} with {symptoms}\?'), 'Sport_with_Symptom') 
 
-    patterns = [Recipe_by_Health, Sport_and_not_Sport, Sport_promotes_over_Sport, Recipe_fuels_Sport, Allergy_eat_Recipe, Recipe_help_Symptom, Sport_with_Symptom]
+    patterns = [Recipe_by_Health, not_Sport_and_Sport, Sport_promotes_over_Sport, Recipe_fuels_Sport, Allergy_eat_Recipe, Recipe_help_Symptom, Sport_with_Symptom]
 
     def __init__(self):
         self.my_world = World()
@@ -29,7 +29,7 @@ class OntologyCheck:
         )  # reasoner is started and synchronized here)  # reasoner is started and synchronized here
         self.graph = self.my_world.as_rdflib_graph()
 
-    def ontology_check_truth(self, user_input: str):
+    def ontology_check_truth(self, user_input: str, verbose = True):
         for pattern in self.patterns: 
             if p := pattern[0].match(user_input):
                 arg1 = '_'.join(p.groups()[0].split(' '))
@@ -38,6 +38,7 @@ class OntologyCheck:
                 else: 
                     arg2 = '_'.join(p.groups()[1].split(' '))
                 if self.check_if_in_ontology(pattern[1], arg1, arg2):
+                    if verbose: print(pattern[1])
                     return query_functions[pattern[1]](self.graph, arg1, arg2)
         raise ValueError(f"Unknown query: {user_input}")
 
